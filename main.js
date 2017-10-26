55f0070cd13c65d462759e4d29e1874d561e97ad
Mike Feldstein <msfeldstein@gmail.com>
2017-10-25 20:17:13 -0700

    Make lib classes available for import

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 module.exports = {
+  GLB: require('./lib/glb'),
   GLBPacker: require('./lib/glb-packer'),
-  GLBParser: require('./lib/glb-parser')
+  GLBParser: require('./lib/glb-parser'),
+  GLBWriter: require('./lib/glb-writer')
 }
